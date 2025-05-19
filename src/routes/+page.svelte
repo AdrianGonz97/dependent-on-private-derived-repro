4748cdada1b5e2d3e15a959b304d1b9f2a90b57e
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-05-19 19:12:43 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	class SomeState {
+		opts: { baz: boolean };
+		#foo = $derived.by(() => this.opts.baz);
+		bar = $derived.by(() => ({ foo: this.#foo }));
+
+		constructor(opts: { baz: boolean }) {
+			this.opts = opts;
+		}
+	}
+
+	let some_state = new SomeState({ baz: true });
+</script>
+
+{some_state.bar.foo}
