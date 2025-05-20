16be6e7dd12f7f4b1c39da4c1fc315296f6b9a27
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2025-05-19 22:24:06 -0400

    simplify further

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	class SomeState {
 		opts: { baz: boolean };
 		#foo = $derived.by(() => this.opts.baz);
-		#bar = $derived.by(() => ({ foo: this.#foo }));
 
 		constructor(opts: { baz: boolean }) {
 			this.opts = opts;
